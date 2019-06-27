38c422b1aae56778ee6f801291ae5a1c0e36552c
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2019-06-27 17:05:28 -0300

    Plugin "eac_redmine_usability".

---
+++ init.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require 'redmine'
+
+Redmine::Plugin.register :eac_redmine_usability do
+  name 'E.A.C.\'s Redmine Usability'
+  author 'Eduardo Henrique Bogoni'
+  description ''
+  version '0.0.0'
+end
