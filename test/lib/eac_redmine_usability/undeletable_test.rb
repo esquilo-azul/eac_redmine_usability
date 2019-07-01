2f25f98d782673cc9644967a87ed7100494bdff9
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2019-07-01 18:58:29 -0300

    Block wiki page deletion.

--- test/lib/eac_redmine_usability/undeletable_test.rb
+++ test/lib/eac_redmine_usability/undeletable_test.rb
@@ -4,9 +4,9 @@ require 'test_helper'
 
 module EacRedmineUsability
   class UndeletableTest < ActiveSupport::TestCase
-    fixtures :issues, :issue_statuses, :users, :wikis
+    fixtures :issues, :issue_statuses, :users, :wikis, :wiki_pages
 
-    [::Issue, ::User, ::Wiki].each do |klass|
+    [::Issue, ::User, ::Wiki, ::WikiPage].each do |klass|
       test "#{klass.name.humanize.downcase} cannot be deleted" do
         cannot_be_deleted_test(klass)
       end
