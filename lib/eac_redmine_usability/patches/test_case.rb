4a04356872f8198d0ae9c553f8493867373044d7
Eduardo Henrique Bogoni <eduardo.bogoni@trf1.jus.br>
2020-08-12 14:22:49 -0300

    EacRedmineUsability::Patches::TestCase: do not require "test_helper".

--- lib/eac_redmine_usability/patches/test_case.rb
+++ lib/eac_redmine_usability/patches/test_case.rb
@@ -19,7 +19,6 @@ module EacRedmineUsability
 end
 
 if ::Rails.env.test?
-  require Rails.root.join('test', 'test_helper.rb')
   patch = ::EacRedmineUsability::Patches::TestCase
   target = ::ActiveSupport::TestCase
   target.send(:include, patch) unless target.included_modules.include?(patch)
