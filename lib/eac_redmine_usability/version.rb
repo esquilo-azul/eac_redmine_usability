0e2618f864a4804a93e6ccf217d64753dcc1f2c0
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2022-07-06 15:06:04 -0300

    Bump to '0.3.3'.

--- lib/eac_redmine_usability/version.rb
+++ lib/eac_redmine_usability/version.rb
@@ -3,7 +3,7 @@
 module EacRedmineUsability
   SLUG = 'eac_redmine_usability'
   NAME = 'E.A.C.\'s Redmine Usability'
-  VERSION = '0.3.2'
+  VERSION = '0.3.3'
   AUTHOR = 'Eduardo Henrique Bogoni'
   SUMMARY = ''
 end
