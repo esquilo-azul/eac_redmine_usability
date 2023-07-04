6bd689584fd7b5bda4f4313636863ea07440f1dc
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2023-07-04 12:01:51 -0300

    RSpec.describe(EacRedmineUsability::Undeletable): convert from EacRedmineUsability::UndeletableTest.

--- lib/tasks/eac_redmine_usability.rake
+++ lib/tasks/eac_redmine_usability.rake
@@ -1,18 +1,9 @@
 # frozen_string_literal: true
 
-require 'rake/testtask'
+require 'redmine_plugins_helper/plugin_rake_task'
+::RedminePluginsHelper::PluginRakeTask.register(:eac_redmine_usability, :test)
 
 namespace :eac_redmine_usability do
-  Rake::TestTask.new(test: 'db:test:prepare') do |t|
-    plugin_root = ::File.dirname(::File.dirname(__dir__))
-
-    t.description = 'Run plugin eac_redmine_usability\'s tests.'
-    t.libs << 'test'
-    t.test_files = FileList["#{plugin_root}/test/**/*_test.rb"]
-    t.verbose = false
-    t.warning = false
-  end
-
   desc 'Close projects no longer used.'
   task :close_unused_projects, %i[expire_days confirm] => :environment do |_t, args|
     ::EacRedmineUsability::CloseUnusedProjects.new(
---
+++ spec/lib/eac_redmine_usability/undeletable_spec.rb
@@ -0,0 +1,32 @@
+# frozen_string_literal: true
+
+::RSpec.describe ::EacRedmineUsability::Undeletable do
+  fixtures :issues, :issue_statuses, :projects, :users, :wikis, :wiki_pages
+
+  around do |example|
+    described_class.on_allow_destroy(false, &example)
+  end
+
+  [::Issue, ::Project, ::User, ::Wiki, ::WikiPage].each do |model|
+    context "when model is #{model}" do
+      let(:record) { model.first }
+
+      it { expect(record).to be_present }
+      it { expect(record.errors[:base]).to be_blank }
+
+      context 'when record is called for detroy' do
+        let(:record_id) { record.id }
+        let(:destroy_result) { record.destroy }
+
+        before do
+          record_id
+          destroy_result
+        end
+
+        it { expect(destroy_result).to be_falsy }
+        it { expect(record.errors[:base]).to be_present }
+        it { expect(model.find(record_id)).to be_present }
+      end
+    end
+  end
+end
